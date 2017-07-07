c87ff2d480b52d471761c98b7973d0e99914f9df
qushaohua <1531553694@qq.com>
2017-07-07 10:51:42 +0800

    bcd_one

--- Android5/app/src/main/java/com/xiao/android5/MainActivity.java
+++ Android5/app/src/main/java/com/xiao/android5/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         password = (TextInputLayout) findViewById(R.id.password);
         EditText editText = username.getEditText();
         editText.addTextChangedListener(this);
-        String abc = "xyz";
+        String abc = "157716454654654";
+        String abcef = "瞿少华";
 
         tab = (TabLayout) findViewById(R.id.tab);
 //        tab.addTab(tab.newTab().setText("哈哈"));
