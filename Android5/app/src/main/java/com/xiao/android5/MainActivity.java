fd0a2b344ef985d9ee6e6333b632d23ee91b7f03
qushaohua <1531553694@qq.com>
2017-07-07 09:34:35 +0800

    main更改

--- Android5/app/src/main/java/com/xiao/android5/MainActivity.java
+++ Android5/app/src/main/java/com/xiao/android5/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         password = (TextInputLayout) findViewById(R.id.password);
         EditText editText = username.getEditText();
         editText.addTextChangedListener(this);
-
+        String abc = "测试专用";
 
         tab = (TabLayout) findViewById(R.id.tab);
 //        tab.addTab(tab.newTab().setText("哈哈"));
